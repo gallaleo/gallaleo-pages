da73090e2182d21a7909c62dd056cd973f056f49
Leonard Gallant <leonard.gallant@gmail.com>
2024-10-16 08:34:04 -0400

    Quartz sync: Oct 16, 2024, 8:34 AM

--- content/Homiles/index.md
+++ content/Homiles/index.md
@@ -3,6 +3,6 @@
 # Homilies
 
 
-- [[./homiles/20240915 - Year B - Ordinary Time - 24th Sunday|20240915 - Year B - Ordinary Time - 24th Sunday]]
-- [[./homiles/20240818 - Year B - Ordinary Time - 20th Sunday|20240818 - Year B - Ordinary Time - 20th Sunday]]
+- [[20240915 - Year B - Ordinary Time - 24th Sunday|20240915 - Year B - Ordinary Time - 24th Sunday]]
+- [[20240818 - Year B - Ordinary Time - 20th Sunday|20240818 - Year B - Ordinary Time - 20th Sunday]]
 
--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
@@ -78,7 +78,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      Plugin.FolderPage(),
+//      Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
